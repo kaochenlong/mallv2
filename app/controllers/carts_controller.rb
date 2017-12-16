6cbb095c773f2a0788fe4308e12f9391f7d4cb1a
Eddie Kao <eddie@5xruby.tw>
2017-12-16 13:57:25 +0800

    add empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,4 +6,9 @@ class CartsController < ApplicationController
 
     redirect_to products_path, notice: "商品已加入購物車!"
   end
+
+  def destroy
+    session[:cart9527] = nil
+    redirect_to products_path, notice: "購物車已清空!"
+  end
 end
